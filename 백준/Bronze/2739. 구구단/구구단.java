22a272942c5089757dff735f6d89d8e368f5ee93
NOH DONG HUN <144079183+nodh020403@users.noreply.github.com>
2026-04-27 23:32:04 +0900

    [Bronze V] Title: 구구단, Time: 128 ms, Memory: 16040 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 1. 입력
+        int N = Integer.parseInt(br.readLine());
+        //int result = 0;
+        for (int i =1; i <=9; i++) {
+            System.out.println(N+" * "+i+" = "+(N*i));
+        }
+    }
+}
