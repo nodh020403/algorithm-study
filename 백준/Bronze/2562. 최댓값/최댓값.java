d59a13fc0721bc733bad0145000f5a400d3a37ad
NOH DONG HUN <144079183+nodh020403@users.noreply.github.com>
2026-04-27 23:32:19 +0900

    [Bronze III] Title: 최댓값, Time: 104 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,33 +1,28 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Stats {
+    int max=0;
+    int number=0;
+    void update(int x, int num) {
+    	if(x>max) {
+    		max=x;
+    		number=num;
+    	}
+    }
+}
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=null;
-
-        //int N = Integer.parseInt(br.readLine());
-        int max=0;
-        int max_seq=0;
-        int temp=0;
-        
-        for (int i = 0; i < 9; i++) {
-        	while (st == null || !st.hasMoreTokens()) {
-        		st = new StringTokenizer(br.readLine());
-        	}
-            temp = Integer.parseInt(st.nextToken());
-            if(i==0) {
-            	max=temp;
-            	max_seq=(i+1);
-            	continue;
-            }
-            if(temp>max) {
-            	max=temp;
-            	max_seq=(i+1);
-            }
-            	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Stats s1 = new Stats();
+		for (int i = 0; i < 9; i++) {
+            s1.update(Integer.parseInt(br.readLine()),(i+1));
         }
-        System.out.println(max);
-        System.out.println(max_seq);
-    }
+		System.out.println(s1.max);
+		System.out.println(s1.number);
+		
+	}
+
 }
