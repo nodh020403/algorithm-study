69b2553221a0959c4fb80d187c4788dea2af9bd5
NOH DONG HUN <144079183+nodh020403@users.noreply.github.com>
2026-04-27 23:31:00 +0900

    [Bronze II] Title: 나머지, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int [] arr= new int[10];
+        for(int i=0;i<10;i++) {
+        	arr[i]= (Integer.parseInt(br.readLine()))%42;
+        }
+        long count=Arrays.stream(arr).distinct().count();
+        
+        System.out.println(count);
+    }
+}
